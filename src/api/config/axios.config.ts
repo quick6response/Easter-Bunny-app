05c0dc6288336c30658d21c710b9afada2cc2881
quick6response <64421792+quick6response@users.noreply.github.com>
2023-03-28 21:22:27 +0300

    конфиги

--- src/api/config/axios.config.ts
+++ src/api/config/axios.config.ts
@@ -7,7 +7,7 @@ export const configAxiosAuth = axios.create({
   baseURL: import.meta.env.PROD
     ? import.meta.env.VITE_API_URL
     : import.meta.env.VITE_API_URL_DEV,
-  withCredentials: true,
+  withCredentials: false,
   timeout: 60 * 1000,
   timeoutErrorMessage:
     'Превышено время ожидания ответа от сервера. Повторите попытку позже.',
