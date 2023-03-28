128250d64802b23c542bc0200a0a33974ec5048f
quick6response <64421792+quick6response@users.noreply.github.com>
2023-03-28 22:13:08 +0300

    Правки

--- src/components/screens/PostInfo/PostInfoComponent.tsx
+++ src/components/screens/PostInfo/PostInfoComponent.tsx
@@ -1,6 +1,7 @@
 import { CommentComponent } from '@components/UI/Comment/CommentComponent';
 import { PostComponent } from '@components/UI/Post/PostComponent';
 import { PostModel } from '@models/post.model';
+import { Group } from '@vkontakte/vkui';
 import { FC } from 'react';
 import { fakeComments } from '../../../fakeData/fake.comments';
 
@@ -8,10 +9,10 @@ export const PostInfoComponent: FC<{ post?: PostModel }> = ({ post }) => {
   if (!post) return <div>Информации о посте не найдено</div>;
 
   return (
-    <>
+    <Group>
       <PostComponent {...post}>
         <CommentComponent comments={fakeComments} />
       </PostComponent>
-    </>
+    </Group>
   );
 };
