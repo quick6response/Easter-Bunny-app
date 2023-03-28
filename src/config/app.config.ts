f342588cf5391996b2f064410c47080e385faea5
quick6response <64421792+quick6response@users.noreply.github.com>
2023-03-28 21:22:22 +0300

    конфиги

---
+++ src/config/app.config.ts
@@ -0,0 +1,5 @@
+export const appConfig = {
+  version: import.meta.env.VITE_APP_CONFIG_VERSION || '0.0 beta 0.0',
+  appUrl: 'https://vk.com/app51585569',
+  groupId: 212_921_812,
+} as const;
---
+++ src/config/link.config.ts
@@ -0,0 +1,5 @@
+export const linkConfig = {
+  groupITHub: 'https://vk.com/ithube',
+  groupCallId: 'https://vk.com/id_called',
+  rulesPost: 'https://vk.com/@eggs_app-pravila-publikacii-zapisei',
+} as const;
