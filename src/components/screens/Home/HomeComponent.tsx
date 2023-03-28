169210779ab02cfc707adda42593eac371a1db2c
quick6response <64421792+quick6response@users.noreply.github.com>
2023-03-28 22:00:09 +0300

    Вынесли код в компоненты

--- src/components/screens/Home/HomeComponent.tsx
+++ src/components/screens/Home/HomeComponent.tsx
@@ -1,30 +1,13 @@
 import { PanelHeaderMainPanel } from '@components/UI/PanelHeader/PanelHeaderMainPanel';
-import { PostComponent } from '@components/UI/Post/PostComponent';
-import { useRouterPopout } from '@hooks/useRouterPopout';
-import { ModalTypes } from '@routes/structure.modal';
-import { utilsService } from '@services/utils/utils.service';
-import { CellButton, Footer, List } from '@vkontakte/vkui';
+import { PostsComponent } from '@components/UI/Post/PostsComponent';
 import { FC } from 'react';
 import { fakePost } from '../../../fakeData/fake.post';
 
 export const HomeComponent: FC = () => {
-  const { pushParameter } = useRouterPopout();
   return (
     <>
       <PanelHeaderMainPanel name={'Главная'} />
-      <CellButton
-        onClick={() => pushParameter('modal', ModalTypes.POST_CREATE)}
-      >
-        Создать пост
-      </CellButton>
-      <List>
-        {fakePost.map((post) => (
-          <PostComponent key={post.id} {...post} />
-        ))}
-      </List>
-      <Footer>
-        {utilsService.declOfNum(fakePost.length, ['пост', 'поста', 'постов'])}
-      </Footer>
+      <PostsComponent posts={fakePost} />
       <div
         style={{
           height:
