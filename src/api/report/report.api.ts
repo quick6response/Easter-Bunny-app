fc918d39d20ee9ddd15e4532effffc9a93b8a3c8
quick6response <64421792+quick6response@users.noreply.github.com>
2023-04-06 23:30:43 +0300

    fix

--- src/api/report/report.api.ts
+++ src/api/report/report.api.ts
@@ -3,14 +3,14 @@ import { IRequest } from '@api/types/request.types';
 import { PostReportModel } from '@models/post.model';
 
 export const ReportApi = {
-  photo: async (photoId: string) => {
+  photo: async (photoId: string | number) => {
     const reportPost = await axiosInstance.post<IRequest<PostReportModel>>(
       `/photos/${photoId}/report`,
     );
 
     return reportPost.data.data;
   },
-  post: async (hash: string) => {
+  post: async (hash: string | number) => {
     const reportPost = await axiosInstance.post<IRequest<PostReportModel>>(
       `/walls/${hash}/report`,
     );
