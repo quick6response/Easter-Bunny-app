8607ccd55ef582b6ecba0d54336fa8b841a9066a
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-03-24 01:10:56 +0300

    update strcuture HomeComponent

--- src/components/screens/Home/HomeComponent.tsx
+++ src/components/screens/Home/HomeComponent.tsx
@@ -1,7 +1,9 @@
 import { useGetWallPosts } from '@api/posts/hooks/useGetWallPosts';
 import { PanelHeaderTabs } from '@components/UI/PanelHeader';
 import { PostsComponent } from '@components/UI/Post/PostsComponent';
+import { useAction } from '@hooks/useActions';
 import { useAppSelector } from '@hooks/useAppSelector';
+import { THomeTab, wallPanelSliceActions } from '@store/wall/wall.panel.slice';
 import { useQueryClient } from '@tanstack/react-query';
 import {
   Button,
@@ -11,12 +13,27 @@ import {
   PullToRefresh,
   Spinner,
 } from '@vkontakte/vkui';
-import { FC, memo, useEffect, useMemo, useState } from 'react';
+import {
+  FC,
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import { useInView } from 'react-intersection-observer';
+
+// чем больше, то быстрее после второго нажатия произойдет переход в верхную часть экрана
+const timeoutAutoScrollTop = 300;
 
 export const HomeComponent: FC = memo(() => {
   const queryClient = useQueryClient();
   const activeTab = useAppSelector((state) => state.wallPanel.tab);
+  const wallPanelAction = useAction(wallPanelSliceActions);
+  const clickTimeout = useRef<null | NodeJS.Timeout>(null);
+  const lastClickTime = useRef(0);
+
   const [isFetchingComponent, setIsFetchingComponent] = useState(false);
   const {
     data,
@@ -42,36 +59,105 @@ export const HomeComponent: FC = memo(() => {
   useEffect(() => {
     if (!isFetchingComponent) return;
     // only refetch the first page
-    queryClient.removeQueries({
-      queryKey: ['posts'],
+
+    const invalidateAndFetchData = async () => {
+      try {
+        // Удаление и сброс значений в кеше
+        // TODO выбрать вид, как будут обновлять посты в ленты 0- они пропадают и появляется лоудер или они остаются
+        queryClient.removeQueries({
+          queryKey: ['posts'],
+        });
+        await queryClient.resetQueries({
+          queryKey: ['posts'],
+        });
+        queryClient.removeQueries({
+          queryKey: ['wall'],
+        });
+        await queryClient.resetQueries({
+          queryKey: ['wall'],
+        });
+
+        // Удаление и сброс значений в кеше
+        // await queryClient.invalidateQueries({ queryKey: ['posts'] });
+        // await queryClient.invalidateQueries({ queryKey: ['wall'] });
+
+        // Выполнение запроса на получение данных
+        await refetch();
+
+        // Установка нового значения в setIsFetchingComponent
+      } catch (error) {
+        // Обработка ошибки
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    invalidateAndFetchData().finally(() => {
+      setIsFetchingComponent(false);
     });
-    queryClient.resetQueries({
-      queryKey: ['posts'],
-    });
-    // console.log('Обновляю всю ленту 1');
-    refetch({});
-    setIsFetchingComponent(false);
-  }, [isFetchingComponent]);
+  }, [isFetchingComponent, queryClient, refetch, setIsFetchingComponent]);
 
   useEffect(() => {
     // console.log(inView);
     if (inView && hasNextPage) {
+      console.log('запрос еще записей', { inView, hasNextPage });
       fetchNextPage();
       // console.log('запрашиваю еще записи');
     }
-  }, [inView]);
+  }, [inView, hasNextPage]);
+
+  const onClickTab = useCallback(
+    (tab: THomeTab) => {
+      // console.log(tab);
+      if (activeTab === tab) {
+        const currentTime = Date.now();
+        const interval = currentTime - lastClickTime.current;
+        if (interval <= timeoutAutoScrollTop) {
+          if (clickTimeout.current) clearTimeout(clickTimeout?.current);
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+        } else {
+          clickTimeout.current = setTimeout(() => {
+            if (clickTimeout.current) clearTimeout(clickTimeout.current);
+          }, timeoutAutoScrollTop);
+        }
+        lastClickTime.current = currentTime;
+        return;
+      }
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+      return wallPanelAction.setTab(tab);
+    },
+    [activeTab],
+  );
 
   if (isError)
     return (
-      <PanelHeaderTabs>
+      <PanelHeaderTabs
+        onClickTab={onClickTab}
+        onRefresh={() => setIsFetchingComponent(true)}
+      >
         <Group>
           <Placeholder>Произошла ошибка получения записей.</Placeholder>
         </Group>
       </PanelHeaderTabs>
     );
 
+  const isReferenceDiv = useMemo(() => {
+    return !(isFetchingNextPage || isLoading);
+  }, [isFetchingNextPage, isLoading]);
+
+  console.log({ isReferenceDiv, isFetchingNextPage, isLoading });
   return (
-    <PanelHeaderTabs>
+    <PanelHeaderTabs
+      onClickTab={onClickTab}
+      onRefresh={() => setIsFetchingComponent(true)}
+    >
       <PullToRefresh
         onRefresh={() => setIsFetchingComponent(true)}
         isFetching={isFetchingComponent}
@@ -106,7 +192,7 @@ export const HomeComponent: FC = memo(() => {
             <Spinner size="regular" style={{ margin: '20px 0' }} />
           </div>
         )}
-        <div ref={ref} />
+        <div ref={isReferenceDiv ? ref : undefined} />
       </PullToRefresh>
     </PanelHeaderTabs>
   );
