de63667109da2046c7776415d7028bffe7428b2e
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-03-24 01:13:11 +0300

    update structure Header

--- src/components/UI/PanelHeader/PanelHeaderTabs.tsx
+++ src/components/UI/PanelHeader/PanelHeaderTabs.tsx
@@ -1,10 +1,18 @@
-import { TabsTypeWallComponent } from '@components/UI/Tabs/TabsTypeWallComponent';
-import { useAction } from '@hooks/useActions';
+import {
+  ITabs,
+  TabsTypeWallComponent,
+} from '@components/UI/Tabs/TabsTypeWallComponent';
 import { useAppSelector } from '@hooks/useAppSelector';
 import { useRouterPopout } from '@hooks/useRouterPopout';
 import { ModalsElement } from '@routes/structure.modal';
-import { THomeTab, wallPanelSliceActions } from '@store/wall/wall.panel.slice';
-import { Icon24Add } from '@vkontakte/icons';
+import { THomeTab } from '@store/wall/wall.panel.slice';
+import {
+  Icon16Camera,
+  Icon16Crown,
+  Icon16HistoryBackwardOutline,
+  Icon16ThumbsUpOutline,
+  Icon24Add,
+} from '@vkontakte/icons';
 import {
   Div,
   FixedLayout,
@@ -15,46 +23,63 @@ import {
   useAdaptivityConditionalRender,
   usePlatform,
 } from '@vkontakte/vkui';
-import { FC, PropsWithChildren, useRef } from 'react';
+import { FC, Fragment, PropsWithChildren } from 'react';
 
-export const PanelHeaderTabs: FC<PropsWithChildren> = ({ children }) => {
-  const wallPanelAction = useAction(wallPanelSliceActions);
+const allTabs: ITabs[] = [
+  {
+    name: 'Все записи',
+    tab: 'all',
+    icon: <Icon16HistoryBackwardOutline />,
+  },
+  {
+    name: 'Популярное',
+    tab: 'top',
+    icon: <Icon16ThumbsUpOutline />,
+  },
+  {
+    name: 'Сделала AI',
+    tab: 'ai',
+    icon: <Icon16Camera />,
+  },
+  {
+    name: 'Закрепленные',
+    tab: 'pin',
+    icon: <Icon16Crown />,
+  },
+];
+
+const getName = (tab: THomeTab) => {
+  switch (tab) {
+    case 'all': {
+      return 'Все записи';
+    }
+    case 'top': {
+      return 'Популярное';
+    }
+    case 'ai': {
+      return 'Сделала AI';
+    }
+    case 'pin': {
+      return 'Закрепленные';
+    }
+  }
+};
+
+interface PanelHeaderTabsPropertiesInterface {
+  onRefresh: () => void;
+  onClickTab: (tab: THomeTab) => void;
+}
+
+export const PanelHeaderTabs: FC<
+  PropsWithChildren<PanelHeaderTabsPropertiesInterface>
+> = ({ children, onClickTab, onRefresh }) => {
   const platform = usePlatform();
   const activeTab = useAppSelector((state) => state.wallPanel.tab);
   const { pushParameter } = useRouterPopout();
 
-  const clickTimeout = useRef<null | NodeJS.Timeout>(null);
-  const lastClickTime = useRef(0);
-
   const { viewWidth } = useAdaptivityConditionalRender();
 
-  const onClickTab = (tab: THomeTab) => {
-    if (activeTab === tab) {
-      const currentTime = Date.now();
-      const interval = currentTime - lastClickTime.current;
-      if (interval <= 200) {
-        if (clickTimeout.current) clearTimeout(clickTimeout?.current);
-        window.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: 'smooth',
-        });
-      } else {
-        clickTimeout.current = setTimeout(() => {
-          if (clickTimeout.current) clearTimeout(clickTimeout.current);
-        }, 200);
-      }
-      lastClickTime.current = currentTime;
-      return;
-    }
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
-    return wallPanelAction.setTab(tab);
-  };
-
+  // todo добавить кнопку обновить сейчас ибо мышкой неудобно
   if (platform === Platform.VKCOM)
     return (
       <>
@@ -74,7 +99,11 @@ export const PanelHeaderTabs: FC<PropsWithChildren> = ({ children }) => {
             Пасхальная лента
           </PanelHeader>
           <Group>
-            <TabsTypeWallComponent onClick={onClickTab} select={activeTab} />
+            <TabsTypeWallComponent
+              onClick={onClickTab}
+              select={activeTab}
+              tabs={allTabs}
+            />
           </Group>
         </FixedLayout>
         <Div style={{ height: 90 }} />
@@ -82,21 +111,31 @@ export const PanelHeaderTabs: FC<PropsWithChildren> = ({ children }) => {
       </>
     );
 
+  // todo добавить кнопку обновить сейчас ибо мышкой неудобно
   return (
     <>
       <PanelHeader
         separator={false}
         before={
-          <PanelHeaderButton aria-label="Открыть окно создание записи">
-            <Icon24Add
-              onClick={() => pushParameter('modal', ModalsElement.POST_CREATE)}
-            />
-          </PanelHeaderButton>
+          <Fragment>
+            <PanelHeaderButton aria-label="Открыть окно создание записи">
+              <Icon24Add
+                onClick={() =>
+                  pushParameter('modal', ModalsElement.POST_CREATE)
+                }
+              />
+            </PanelHeaderButton>
+          </Fragment>
         }
       >
-        Пасхальная лента
+        <div onClick={() => onClickTab(activeTab)}>Пасхальная лента </div>
       </PanelHeader>
-      <TabsTypeWallComponent onClick={onClickTab} select={activeTab} />
+      {/*// todo нужно зафиксировать элемент в верхней части экрана*/}
+      <TabsTypeWallComponent
+        onClick={onClickTab}
+        select={activeTab}
+        tabs={allTabs}
+      />
       {children}
     </>
   );
