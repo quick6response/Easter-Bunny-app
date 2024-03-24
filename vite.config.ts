55ff5d17c84d91090c5811125b584189475f7d7c
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-03-24 19:04:35 +0300

    update confix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import * as path from 'node:path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
