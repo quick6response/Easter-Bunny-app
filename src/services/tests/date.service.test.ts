79a4facb0808d183ed02096a7e7c3924dc3e4749
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-03-24 21:08:42 +0300

    add new tests

--- src/services/tests/date.service.test.ts
+++ src/services/tests/date.service.test.ts
@@ -40,6 +40,44 @@ describe('DateService', () => {
 
       // Проверяем, что в выводе нет "сегодня в"
       expect(formattedDate).not.toContain('сегодня в');
+      // Проверяем, что в выводе нет "вчера в"
+      expect(formattedDate).not.toContain('вчера в');
+      // Проверяем ожидаемый формат даты и времени
+      expect(formattedDate).toBe(expectedString);
+    });
+
+    test('convertDateAndTimeToFormat for a date one month ago', () => {
+      const oneMonthAgo = new Date();
+      oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+
+      const formattedDate = dateService.convertDateAndTimeToFormat(
+        oneMonthAgo.toString(),
+      );
+      const expectedString = `${dateService.convertDateToFormat(oneMonthAgo)} в ${dateService.convertTimeToFormat(oneMonthAgo.toString())}`;
+
+      // Проверяем, что в выводе нет "сегодня в"
+      expect(formattedDate).not.toContain('сегодня в');
+      // Проверяем, что в выводе нет "вчера в"
+      expect(formattedDate).not.toContain('вчера в');
+      // Проверяем ожидаемый формат даты и времени
+      expect(formattedDate).toBe(expectedString);
+    });
+
+    test('convertDateAndTimeToFormat for a date one month and year ago', () => {
+      const oneMonthAndYearAgo = new Date();
+      oneMonthAndYearAgo.setMonth(oneMonthAndYearAgo.getMonth() - 1);
+      oneMonthAndYearAgo.setFullYear(oneMonthAndYearAgo.getFullYear() - 1);
+
+      const formattedDate = dateService.convertDateAndTimeToFormat(
+        oneMonthAndYearAgo.toString(),
+      );
+      // eslint-disable-next-line max-len
+      const expectedString = `${dateService.convertDateToFormat(oneMonthAndYearAgo)} в ${dateService.convertTimeToFormat(oneMonthAndYearAgo.toString())}`;
+
+      // Проверяем, что в выводе нет "сегодня в"
+      expect(formattedDate).not.toContain('сегодня в');
+      // Проверяем, что в выводе нет "вчера в"
+      expect(formattedDate).not.toContain('вчера в');
       // Проверяем ожидаемый формат даты и времени
       expect(formattedDate).toBe(expectedString);
     });
